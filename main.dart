8f91ebffd2699574103a4e7e26410feb591d1600
Mohamed25885 <memo_868@yahoo.com>
2022-01-24 12:42:44 +0200

    read path from console

--- main.dart
+++ main.dart
@@ -4,15 +4,19 @@ import 'dart:io';
 import 'scanLanugage.dart';
 
 void main(List<String> args) async {
- final Map<String, String> jsonFile = {};
- ScanLanguage scan = ScanLanguage();
- final files = await scan.entitiesToFiles('path');
- for (var file in files) {
-   final lines = await file.readAsLines();
-   for (var line in lines) {
-     final matches =  await scan.captureMatches(line);
-     jsonFile.addAll(matches);
-   }
- }
- await File("default.json").writeAsString(jsonEncode(jsonFile));
+  print("Enter the path to scan: ");
+  final path = stdin.readLineSync(encoding: utf8);
+  if (path == null || path.isEmpty) exit(1);
+  final Map<String, String> jsonFile = {};
+  ScanLanguage scan = ScanLanguage();
+  final files = await scan.entitiesToFiles(path);
+  for (var file in files) {
+    final lines = await file.readAsLines();
+    for (var line in lines) {
+      final matches = await scan.captureMatches(line);
+      jsonFile.addAll(matches);
+    }
+  }
+  await File("default.json").writeAsString(jsonEncode(jsonFile));
+  print(jsonFile.length);
 }
